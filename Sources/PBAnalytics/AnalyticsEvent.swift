a9dfddc0195a3174442a1f6032d055e1a717a3c5
Hasan Can Aslan <aslan4044@gmail.com>
2021-07-28 21:24:58 +0300

    feat: add new AnalyticsEvent init functions

--- Sources/PBAnalytics/AnalyticsEvent.swift
+++ Sources/PBAnalytics/AnalyticsEvent.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 
+// MARK: - AnalyticsEvent
+
 @propertyWrapper
 public struct AnalyticsEvent: AnalyticsEventProtocol {
     public var name: String
     public var params: [String: Any] = [:]
 
     public var wrappedValue: AnalyticsEvent {
-        return AnalyticsEvent(name: name,
-                              params: params)
+        return AnalyticsEvent(
+            name: name,
+            params: params)
     }
 }
 
@@ -24,10 +27,24 @@ public extension AnalyticsEvent {
     }
 
     init(screen: String, event: String) {
-        self.init(name: "\(screen)_\(event)")
+        self.init(name: "\(screen)-\(event)")
+    }
+
+    init(event: String, params: [String: Any]) {
+        self.init(name: "\(event)", params: params)
+    }
+
+    init(screen: String, event: String, params: [String: Any]) {
+        self.init(name: "\(screen)-\(event)", params: params)
     }
 
     init(screen: String, button: String) {
-        self.init(name: "\(screen)_\(button)_Tapped")
+        self.init(name: "\(screen)-\(button) Tapped")
+    }
+
+    init(screen: String, button: String, clickValue: String) {
+        self.init(
+            name: "\(screen)-\(button) Tapped-\(clickValue)",
+            params: ["click_value": clickValue])
     }
 }
